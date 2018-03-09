04b43154d8ea6187c2bef0fa19da0390dd75b42e
REM_ <ultimate.spidey.18@gmail.com>
2018-03-08 19:29:39 -0600

    First Commit
    Added the project, some basics and art

---
+++ CuidaElAgua_Unity/Assets/Cuida_El_Agua_Main/Scripts/CamController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour {
+
+	[Header("ROTATION VARIABLES")]
+	public float speed = 5.0f;
+	public Transform target;
+	public Transform x_axis;
+
+	[Header("ZOOM VARIABLES")]
+	public float minFov = 15f;
+	public float maxFov = 90f;
+	public float sensitivity = 10f;
+
+	void Update () {
+
+		CamYRotation ();
+		//Zoom ();
+			
+	}
+
+
+	void CamYRotation(){
+		if (Input.GetMouseButton(0)) {
+			transform.LookAt(target);
+			transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X")*speed);
+
+		}
+	}
+
+	void Zoom () {
+		float fov = Camera.main.fieldOfView;
+		fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+		fov = Mathf.Clamp(fov, minFov, maxFov);
+		Camera.main.fieldOfView = fov;
+	}
+
+}
