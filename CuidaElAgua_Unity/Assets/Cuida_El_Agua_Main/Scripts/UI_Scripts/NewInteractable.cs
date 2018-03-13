c56e16930a172353b1ece6034cbac64e3b275e73
REM_ <ultimate.spidey.18@gmail.com>
2018-03-13 09:38:49 -0600

    Updated 2.5 D UI

---
+++ CuidaElAgua_Unity/Assets/Cuida_El_Agua_Main/Scripts/UI_Scripts/NewInteractable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class NewInteractable : MonoBehaviour {
+
+	//Button Prefab to instantiate
+	public RectTransform interactableButton;
+	//Main canvas for interactables
+	public RectTransform interactablesCanvas;
+	//Distance from object to player to show button
+	public float interactionDistance;
+	//PLayer reference
+	Transform playerRef;
+	//CamReference
+	Camera mainCam;
+
+	//Variables
+	public Vector3 myPositionOnScreen;
+	public float distanceFromPlayer;
+	public float interactableHeightOffset = 128f;
+	RectTransform buttonRect;
+
+
+	void Awake(){
+		//Gets a cam ref
+		mainCam = Camera.main;
+		//Gets a player reference
+		playerRef = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	void LateUpdate(){
+
+		myPositionOnScreen = mainCam.WorldToScreenPoint (this.transform.position);
+
+		interactableButton.position = new Vector2 (myPositionOnScreen.x, myPositionOnScreen.y + interactableHeightOffset);
+
+		distanceFromPlayer = Vector3.Distance (this.transform.position, playerRef.position);
+		if (distanceFromPlayer <= interactionDistance) {
+			Debug.Log ("Player is close enought to interact with " + this.gameObject.name);
+		}
+	}
+}
